46846d77791f2b8abcc280f98fd46cfe7a81346d
woravich-k <woravich@hotmail.com>
2018-03-06 12:49:58 -0500

    Serve flexible tables query to get GeoJSON

--- httpServer.js
+++ httpServer.js
@@ -92,6 +92,62 @@ app.get('/getPOI', function (req,res) {
 	});
 });
 
+app.get('/getGeoJSON/:tablename/:geomcolumn',function(req,res){
+	pool.connect(function(err,client,done){
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		var colnames = "";
+		
+		//first get a list of the columns that are in the table
+		//use string agg to generate a comma separated list that can then be pasted into the next query
+		var querystring = "select string_agg(colname,',') from (select column_name as colname ";
+		querystring = querystring + " FROM information_schema.columns as colname ";
+		querystring = querystring + " where table_name ='" + req.params.tablename + "'";
+		querystring = querystring + " and column_name <>'" + req.params.geomcolumn +"') as cols ";
+		
+		console.log(querystring);
+		
+		//now run the query
+		client.query(querystring, function(err,result){
+			//call 'done()' to release the client back to the pool
+			console.log("trying");
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			for (var i = 0; i < result.rows.length; i++){
+				console.log(result.rows[i].string_agg);
+			}
+			thecolnames = result.rows[0].string_agg;
+			colnames = thecolnames;
+			console.log("the colnames "+thecolnames);
+			//now use the inbuilt geoJSON functionality and create the required geoJSON format using  a query adapted from here:
+			//http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html
+			//note that query needs  to be a single string with no line breaks so built it upp bit by bit
+			
+			var querystring = "select 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+			querystring = querystring + "(select 'Feature' As type, ST_AsGeoJSON(lg." + req.params.geomcolumn + ")::json As geometry, ";
+			querystring = querystring + "row_to_json((select l from (select "+colnames+ ") As l )) as properties";
+			querystring =querystring + " from " + req.params.tablename + " as lg limit 100) as f ";
+			console.log(querystring);
+			//run the second query
+			client.query(querystring,function(err,result){
+				//call 'done' to release the client back to the pool
+				done();
+				if(err){
+					console.log(err);
+					res.status(400).send(err);
+				}
+				res.status(200).send(result.rows);
+			});
+		});
+	});
+});
+	
+			
 
 	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
   app.get('/:name1', function (req, res) {
